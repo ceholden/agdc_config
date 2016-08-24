e8592d8439ebd977eafc3929b61d73332a1bef67
Chris Holden <ceholden@gmail.com>
2016-08-24 18:19:22 -0400

    Improve unzip helper script

--- utils/1_ESPA_unzip_and_index.sh
+++ utils/1_ESPA_unzip_and_index.sh
@@ -3,7 +3,7 @@
 
 set -e
 
-ROOT=/projectnb/landsat/users/ceholden/2016_DATACUBE/AGDC/TEST_DATA
+# ROOT=/projectnb/landsat/users/ceholden/2016_DATACUBE/AGDC/TEST_DATA
 
 function usage() {
     echo "Usage: $0 <ESPA Product (tar.gz)>"
@@ -22,22 +22,30 @@ fi
 ARCHIVE=$1
 
 # TODO: hard coded prep script
-PREP=/projectnb/landsat/users/ceholden/2016_DATACUBE/AGDC/agdc-v2/utils/usgslsprepare.py
+# PREP=/projectnb/landsat/users/ceholden/2016_DATACUBE/AGDC/agdc-v2/utils/usgslsprepare.py
+PREP=/home/ceholden/Documents/DATACUBE/agdc-v2/utils/usgslsprepare.py
 # TODO: hard coded
-EXTRACT_DIR=$ROOT/extract/
+EXTRACTED=$ROOT/extract
+mkdir -p $EXTRACTED
 # TODO: hard coded 'completed'
 COMPLETED=$ROOT/completed
+mkdir -p $COMPLETED
 
 PRODUCT_ID=$(basename $ARCHIVE .tar.gz)
-EXTRACT_DIR=$EXTRACT_DIR/$PRODUCT_ID
+EXTRACT_DIR=$EXTRACTED/$PRODUCT_ID
 mkdir -p $EXTRACT_DIR
 
-echo "Unzipping"
-tar -C $EXTRACT_DIR -xvf $ARCHIVE
+SCENE_ID=$(basename $(tar -tf $ARCHIVE | grep ".xml") .xml)
 
-echo "Removing DN band images"
-SCENE_ID=$(ls -1 $EXTRACT_DIR/ | head -n 1 | awk -F '_' '{ print $1 }')
-rm -vf $EXTRACT_DIR/${SCENE_ID}_band*.tif
+echo "Unzipping"
+tar -C $EXTRACT_DIR \
+    --keep-old-files \
+    --exclude "${SCENE_ID}_band*" \
+    -xvf $ARCHIVE
+
+# echo "Removing DN band images"
+# SCENE_ID=$(ls -1 $EXTRACT_DIR/ | head -n 1 | awk -F '_' '{ print $1 }')
+# rm -vf $EXTRACT_DIR/${SCENE_ID}_band*.tif
 
 echo "Preparing dataset metadata"
 python $PREP $EXTRACT_DIR/ 
